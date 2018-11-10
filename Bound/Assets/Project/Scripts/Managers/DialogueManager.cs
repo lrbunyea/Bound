02cf067f2db07b785d9c569eb6c56ca839490f8b
Leo Bunyea <lrbunyea@wpi.edu>
2018-11-10 17:10:58 -0500

    Edited narration text

--- Bound/Assets/Project/Scripts/Managers/DialogueManager.cs
+++ Bound/Assets/Project/Scripts/Managers/DialogueManager.cs
@@ -28,15 +28,15 @@ public class DialogueManager : MonoBehaviour {
 
     private void Start()
     {
-        introductionDialogue = new string[] { "“Do not, under any circumstances, wear for more than eight hours a day.”",
+        introductionDialogue = new string[] { "Every day after lunch, I used to fly down this sidewalk and up those stairs to the track.",
+        "My friends were never able to catch up with me so they would just lean back on the chain link fence and watch. Flashing me a thumbs up when I blew by.",
+        "I want that again.",
+        "“Do not, under any circumstances, wear for more than eight hours a day.”",
         "“Do not wear during strenuous physical activity.”",
         "That’s what the package told me.",
         "Then, later, when I googled it, the internet told me the same thing.",
         "So, I said, “Fuck it” and now I’m here where I went to middle school like… eight years ago.",
-        "Wearing this binder that I’m not supposed to and running shoes that I haven’t touched since I came out.",
-        "Every day after lunch, I used to fly down this sidewalk and up those stairs to the track.",
-        "My friends were never able to catch up with me so they would just lean back on the chain link fence and watch. Flashing me a thumbs up when I blew by.",
-        "I want that again. So badly."};
+        "Wearing this binder that I’m not supposed to and running shoes that I haven’t touched since I came out."};
     }
     #endregion
 
