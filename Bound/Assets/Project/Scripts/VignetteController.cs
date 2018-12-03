281051e23b533aef64021c59ad95e54f24b61d95
Leo Bunyea <lrbunyea@wpi.edu>
2018-12-03 11:22:12 -0500

    Added post processing to project - vignette now works!

---
+++ Bound/Assets/Project/Scripts/VignetteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class VignetteController : MonoBehaviour {
+
+    #region Variables
+    [SerializeField] PostProcessingProfile ppp;
+    #endregion
+
+    #region Unity API Functions
+    void Start () {
+        ppp = GetComponent<PostProcessingBehaviour>().profile;
+        GameManager.Instance.IncorrectKeyPress.AddListener(PassOut);
+	}
+    #endregion
+
+    #region Event Functions
+    /// <summary>
+    /// Function that listens for an event that is triggered when the player incorrectly pressed a key
+    /// Increases the size of the vignette on the screen
+    /// </summary>
+    private void PassOut()
+    {
+        VignetteModel.Settings vignetteSettings = ppp.vignette.settings;
+        vignetteSettings.intensity = ppp.vignette.settings.intensity + .2f;
+        ppp.vignette.settings = vignetteSettings;
+    }
+    #endregion
+}
